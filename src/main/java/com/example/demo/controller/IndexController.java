1b62b19b95785b72b06d7cef22d324e1373d61ff
你的名字或昵称 <1209325963@qq.com>
2019-12-06 16:04:52 +0800

    暂时提交

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,18 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Account;
 import com.example.demo.model.BlogProperties;
 import com.example.demo.model.ConfigBean;
 import com.example.demo.model.TestConfigBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description
  * @Author xiaohu
  * @Date 2019/11/27 11:32
  */
-@RestController
+@Controller
 public class IndexController {
 
     @Autowired
@@ -29,7 +35,6 @@ public class IndexController {
         return blogProperties.getName()+"——"+blogProperties.getTitle();
     }
 
-
     @RequestMapping("/test2")
     String test2() {
         return configBean.getName()+"——"+configBean.getTitle()+"——"+configBean.getWholeTitle();
@@ -39,4 +44,5 @@ public class IndexController {
     String test3() {
         return testConfigBean.getName()+"——"+testConfigBean.getAge();
     }
+
 }
